8fde23c134c2b00c1f15b2db3df6c616bbf2e1d4
Otávio Oliveira <76442768+TavoOliveira@users.noreply.github.com>
2024-03-21 19:45:25 -0300

    Deletado pasta javascript

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-let inputTexto = document.querySelector("#input-texto");
-let btnEnviar = document.querySelector("#btn-enviar");
-let saida = document.querySelector("#saida");
-
-function enviarTexto() {
-    saida.textContent = inputTexto.value;
-}
