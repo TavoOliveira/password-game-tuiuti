671211a1a8c51da5843373460be686c93ffeda92
TavoOliveira <otaviooliveira0099@gmail.com>
2024-03-21 22:49:39 -0300

    Deletado

--- python/script.py
+++
@@ -1,7 +0,0 @@
-from pyscript import document
-
-def enviar_texto(event):
-    input_texto = document.querySelector("#input-texto")
-    saida = document.querySelector("#saida")
-            
-    saida.textContent = input_texto.value
